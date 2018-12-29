d4c85bc8ecadc66f0a60cd1d5cdbd33b1591f0b9
Bhulakshmi <bhulakshmi@bitlab.academy>
2018-12-29 16:09:23 +0530

    Initial commit

---
+++ precache-manifest.4e7d9d81d783dc7376d381bb507a2040.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d51c4cbdf58ed633c094cc393e644e66",
+    "url": "/static/media/blackarrow.d51c4cbd.svg"
+  },
+  {
+    "revision": "de291c9ea637745af804",
+    "url": "/static/css/main.15a3d488.chunk.css"
+  },
+  {
+    "revision": "46cd0371d35d6dbe23ad",
+    "url": "/static/js/1.46cd0371.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "291c60c56f8aced6761914bb659d77e6",
+    "url": "/static/media/downarrow.291c60c5.svg"
+  },
+  {
+    "revision": "5ddd83f93083336ea6f13e4b4c0f7461",
+    "url": "/static/media/userprofile.5ddd83f9.svg"
+  },
+  {
+    "revision": "de291c9ea637745af804",
+    "url": "/static/js/main.de291c9e.chunk.js"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "2212b50f69d9b5cbe3f453f030747515",
+    "url": "/static/media/city.2212b50f.svg"
+  },
+  {
+    "revision": "7bf199918881b27a6e9cdb41e828aabc",
+    "url": "/static/media/calendar.7bf19991.svg"
+  },
+  {
+    "revision": "6d133fe402f26b227171f3f155b6a43c",
+    "url": "/static/media/steering.6d133fe4.svg"
+  },
+  {
+    "revision": "6aa0f2833f8af6a73a6d2afc7067b91d",
+    "url": "/index.html"
+  }
+];
